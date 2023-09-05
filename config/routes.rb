3df5ac11208931938839b590aafc9cd5b3ee2208
nathalie <natie.moraga.sch@gmail.com>
2023-09-04 22:48:27 -0300

    portafolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'portafolio/project'
   get 'portafolio/home'
+  get 'portafolio/index'
   get 'portafolio/contact'
+  get 'portafolio/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
